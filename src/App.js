c64152654e64b1c77ecc3356c198e00fb261e087
Viktor Yordanov <87126339+mahlichavpm@users.noreply.github.com>
2022-10-13 00:58:40 +0300

    router added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 
+import { Link, Route, Routes } from "react-router-dom";
+import About from "./components/About";
+import Home from "./components/Home";
 import TodoList from "./components/TodoList";
-
 
 function App() {
   return (
-    <div className="App">
-      <TodoList/>
-    </div>
+    <>
+      <nav>
+        <Link to='/' >Home</Link>
+        <Link to='/todo' >Todo</Link>
+        <Link to='/about' >About us</Link>
+      </nav>
+      <Routes>
+        <Route path="/"  element={<Home/>}/>
+        <Route path="/todo"  element={ <TodoList/>}/>
+        <Route path="/about"  element={<About/>}/>
+      </Routes>  
+    </>
   );
 }
 
